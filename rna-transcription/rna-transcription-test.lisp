e22d07113a91c518a77918d4a9bb35a90d2ae941
Mark Simpson <verdammelt@gmail.com>
2014-08-24 16:30:18 -0400

    Fixing rna-transcription to match readme.
    This fixes exercism/xlisp#3.
    
    and fixes the lisp project specific problem mentioned in:
    exercism/exercism.io#1310.

--- rna-transcription/rna-transcription-test.lisp
+++ rna-transcription/rna-transcription-test.lisp
@@ -7,20 +7,20 @@
 
 (in-package :rna-transcription-test)
 
-(define-test transcribes-cytidine-unchanged
-  (assert-equal "C" (dna:to-rna "C")))
+(define-test transcribes-cytidine-to-guanosine
+  (assert-equal "G" (dna:to-rna "C")))
 
-(define-test transcribes-guanosine-unchanged
-  (assert-equal "G" (dna:to-rna "G")))
+(define-test transcribes-guanosine-to-cytidine
+  (assert-equal "C" (dna:to-rna "G")))
 
-(define-test transcribes-adenosine-unchanged
-  (assert-equal "A" (dna:to-rna "A")))
+(define-test transcribes-adenosine-to-uracile
+  (assert-equal "U" (dna:to-rna "A")))
 
-(define-test it-transcribes-thymidine-to-uracil
-  (assert-equal "U" (dna:to-rna "T")))
+(define-test it-transcribes-thymidine-to-adenosine
+  (assert-equal "A" (dna:to-rna "T")))
 
-(define-test it-transcribes-all-occurrences-of-thymidine-to-uracil
-  (assert-equal "ACGUGGUCUUAA" (dna:to-rna "ACGTGGTCTTAA")))
+(define-test it-transcribes-all-nucleotides
+  (assert-equal "UGCACCAGAAUU" (dna:to-rna "ACGTGGTCTTAA")))
 
 (define-test it-validates-dna-strands
   (assert-error 'error (dna:to-rna "XCGFGGTDTTAA")))
