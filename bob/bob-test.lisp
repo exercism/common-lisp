f334e52397ea5adb306b0395d3f69a1a29ec48e6
Austin Lyons <austinlyons@users.noreply.github.com>
2014-09-24 07:29:06 -0500

    Update bob-test.lisp
    Woah --> Whoa

--- bob/bob-test.lisp
+++ bob/bob-test.lisp
@@ -11,7 +11,7 @@
   (assert-equal "Whatever." (bob:response-for "Tom-ay-to, tom-aaaah-to.")))
 
 (define-test responds-to-shouts
-  (assert-equal "Woah, chill out!" (bob:response-for "WATCH OUT!")))
+  (assert-equal "Whoa, chill out!" (bob:response-for "WATCH OUT!")))
 
 (define-test responds-to-questions
   (assert-equal "Sure." (bob:response-for "Does this cryogenic chamber make me look fat?")))
@@ -23,16 +23,16 @@
   (assert-equal "Whatever." (bob:response-for "It's OK if you don't want to go to the DMV.")))
 
 (define-test responds-to-forceful-questions
-  (assert-equal "Woah, chill out!" (bob:response-for "WHAT THE HELL WERE YOU THINKING?")))
+  (assert-equal "Whoa, chill out!" (bob:response-for "WHAT THE HELL WERE YOU THINKING?")))
 
 (define-test responds-to-shouting-with-special-characters
-  (assert-equal "Woah, chill out!" (bob:response-for "ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!")))
+  (assert-equal "Whoa, chill out!" (bob:response-for "ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!")))
 
 (define-test responds-to-shouting-numbers
-  (assert-equal "Woah, chill out!" (bob:response-for "1, 2, 3 GO!")))
+  (assert-equal "Whoa, chill out!" (bob:response-for "1, 2, 3 GO!")))
 
 (define-test responds-to-shouting-with-no-exclamation-mark
-  (assert-equal "Woah, chill out!" (bob:response-for "I HATE YOU")))
+  (assert-equal "Whoa, chill out!" (bob:response-for "I HATE YOU")))
 
 (define-test responds-to-statement-containing-question-mark
   (assert-equal "Whatever." (bob:response-for "Ending with ? means a question.")))
