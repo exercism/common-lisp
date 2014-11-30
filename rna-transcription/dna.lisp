ea8f7e9159045e9703a2ad1a9d80b8294bc2f358
William Clifford <wclifford@crowdcompass.com>
2014-11-30 13:11:59 -0800

    rna-trans: add template dna.lisp file

---
+++ rna-transcription/dna.lisp
@@ -0,0 +1,9 @@
+(cl:in-package #:cl-user)
+(cl:defpackage #:dna
+  (:use #:cl)
+  (:export #:to-rna))
+(cl:in-package #:dna)
+
+(defun to-rna (str)
+  "Transcribe a string representing DNA nucleotides to RNA."
+  )
