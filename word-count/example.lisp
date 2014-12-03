ef18839843164279ecbdb04f58de8c3acdefa3e7
Mark Simpson <verdammelt@gmail.com>
2014-12-02 21:49:29 -0500

    Word-count: simpler and consistent package definition.

--- word-count/example.lisp
+++ word-count/example.lisp
@@ -1,13 +1,13 @@
-(cl:defpackage #:phrase
-  (:use :common-lisp)
-  (:export :word-count))
+(defpackage #:phrase
+  (:use #:common-lisp)
+  (:export #:word-count))
 
-(in-package :phrase)
+(in-package #:phrase)
 
 (defun split-string (string)
   (labels ((split-string-iter (string result)
 	     (let ((idx (position #\Space string :from-end t)))
-	       (if idx 
+	       (if idx
 		   (split-string-iter (subseq string 0 idx)
 				      (cons (subseq string (1+ idx)) result))
 		   (cons string result)))))
@@ -24,9 +24,9 @@
 	     string))
 
 (defun word-count (sentence)
-  (let ((words (remove-empty-strings 
-		(split-string 
-		 (strip-punctuation 
+  (let ((words (remove-empty-strings
+		(split-string
+		 (strip-punctuation
 		  (string-downcase sentence))))))
     (mapcar #'(lambda (w) (cons w (count w words :test #'string=)))
 	    (remove-duplicates words :test #'string=))))
---
+++ word-count/word-count.lisp
@@ -0,0 +1,5 @@
+(in-package #:cl-user)
+(defpackage #:phrase
+  (:use #:cl)
+  (:export #:word-count))
+(in-package #:phrase)
