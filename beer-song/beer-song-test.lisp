e248ed3010c2d76960a99464433d028617a80989
Mark Simpson <verdammelt@gmail.com>
2014-12-02 22:18:10 -0500

    Beer-song: simpler and consistent package definition.

--- beer-song/beer-song-test.lisp
+++ beer-song/beer-song-test.lisp
@@ -1,30 +1,30 @@
 (ql:quickload "lisp-unit")
 
-(cl:defpackage #:beer-song-test
-  (:use :common-lisp :lisp-unit))
+(defpackage #:beer-song-test
+  (:use #:common-lisp #:lisp-unit))
 
 (load "beer")
 
-(in-package :beer-song-test)
+(in-package #:beer-song-test)
 
-(defparameter +verse-8+ 
+(defparameter +verse-8+
   "8 bottles of beer on the wall, 8 bottles of beer.
 Take one down and pass it around, 7 bottles of beer on the wall.
 ")
-(defparameter +verse-2+ 
+(defparameter +verse-2+
   "2 bottles of beer on the wall, 2 bottles of beer.
 Take one down and pass it around, 1 bottle of beer on the wall.
 ")
-(defparameter +verse-1+ 
+(defparameter +verse-1+
   "1 bottle of beer on the wall, 1 bottle of beer.
 Take it down and pass it around, no more bottles of beer on the wall.
 ")
-(defparameter +verse-0+ 
+(defparameter +verse-0+
   "No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall.
 ")
 
-(defparameter +song-8-6+ 
+(defparameter +song-8-6+
   "8 bottles of beer on the wall, 8 bottles of beer.
 Take one down and pass it around, 7 bottles of beer on the wall.
 
@@ -35,7 +35,7 @@ Take one down and pass it around, 6 bottles of beer on the wall.
 Take one down and pass it around, 5 bottles of beer on the wall.
 
 ")
-(defparameter +song-3-0+ 
+(defparameter +song-3-0+
   "3 bottles of beer on the wall, 3 bottles of beer.
 Take one down and pass it around, 2 bottles of beer on the wall.
 
@@ -62,4 +62,3 @@ Go to the store and buy some more, 99 bottles of beer on the wall.
 (let ((*print-errors* t)
       (*print-failures* t))
   (run-tests :all :beer-song-test))
-
