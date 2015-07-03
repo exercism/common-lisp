9dc307c29430a46dbf7edb2221390fc24e22cc5b
Mark Simpson <verdammelt@gmail.com>
2015-07-03 16:40:54 -0400

    First pass example code.

--- allergies/example.lisp
+++ allergies/example.lisp
@@ -5,5 +5,32 @@
 
 (in-package #:allergies)
 
-(defun list (score))
-(defun allergic-to-p (score allergen))
+(defparameter *allergens-scores*
+  '(("eggs" 1)
+    ("peanuts" 2)
+    ("shellfish" 4)
+    ("strawberries" 8)
+    ("tomatoes" 16)
+    ("chocolate" 32)
+    ("pollen" 64)
+    ("cats" 128)))
+
+(defun allergen (allergen-and-score) (first allergen-and-score))
+(defun score (allergen-and-score) (second allergen-and-score))
+
+(defun score-matches (allergen-and-score score)
+  (not (zerop (logand (score allergen-and-score) score))))
+
+(defun find-all (test sequence)
+  (loop for i in sequence
+     when (funcall test i) collecting i))
+
+(defun list (score)
+  (mapcar #'allergen
+          (find-all
+           #'(lambda (as) (score-matches as score))
+           *allergens-scores*)))
+
+(defun allergic-to-p (score allergen)
+  (score-matches (assoc allergen *allergens-scores* :test #'string=)
+                 score))
