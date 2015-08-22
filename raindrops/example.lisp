c5f97306719ab4972df035b111e69e2b8c7728e9
Mark Simpson <verdammelt@gmail.com>
2015-08-22 15:24:23 -0400

    Example code for Raindrops exercise using DO* Loop

--- raindrops/example.lisp
+++ raindrops/example.lisp
@@ -4,26 +4,14 @@
 
 (in-package #:raindrops)
 
-(defun factor (n)
-  "Return a list of factors of n."
-  (when (> n 1)
-    (loop with max-d = (isqrt n)
-       for d = 2 then (if (evenp d) (+ d 1) (+ d 2)) do
-         (cond ((> d max-d) (return (list n))) ; n is prime
-               ((zerop (rem n d)) (return (cons d (factor (truncate n d)))))))))
-
-(defun convert (n)
-  (let ((drops "")
-        (factors (remove-if-not #'(lambda (v)
-                                    (and (>= v 3) (<= v 7)))
-                                (factor n))))
-    (if (not (null factors))
-        (progn
-          (when (member 3 factors)
-            (setf drops (concatenate 'string drops "Pling")))
-          (when (member 5 factors)
-            (setf drops (concatenate 'string drops "Plang")))
-          (when (member 7 factors)
-            (setf drops (concatenate 'string drops "Plong"))))
-        (setf drops (concatenate 'string (write-to-string n))))
-    drops))
+(defun convert (n &optional (cases '((3 . "Pling") (5 . "Plang") (7 . "Plong"))))
+  (let ((results
+         (do* ((cs cases (cdr cs))
+               (c (car cs) (car cs))
+               (result (list)))
+              ((null cs) (reverse result))
+           (when (zerop (mod n (car c)))
+             (push (cdr c) result)))))
+    (if results
+        (apply #'concatenate 'string results)
+        (write-to-string n))))
