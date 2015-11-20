6e1d4bb7d8ac3efcf07f66d558fff350a52814ae
TatriX <tatrics@gmail.com>
2015-11-20 18:00:47 +0300

    Added test for the uniqueness of robot names

--- robot-name/robot-name-test.lisp
+++ robot-name/robot-name-test.lisp
@@ -35,6 +35,29 @@
 	(robot:robot-name robot)
 	original-name)))
 
+(defparameter *robot-names* (make-hash-table :test 'equal))
+
+(defun test-unique-names ()
+  (loop
+     ;; Full check will be slow but there are enough
+     ;; iterations to detect non-unique names.
+     ;; If in future we will be checking all possible names
+     ;; we should not forget about names that were already used
+     ;; by previous tests
+     with max = (* 26 26 100)
+     with started = (get-universal-time)
+     with max-execution-time = 3 ; seconds
+     for i upto max
+     for name = (robot:robot-name (robot:build-robot))
+     if (gethash name *robot-names*) do
+       (return 'name-collision)
+     if (> (- (get-universal-time) started) max-execution-time) do
+       (return 'implementation-is-slow)
+     do (setf (gethash name *robot-names*) name)))
+
+(define-test names-are-unique ()
+             (assert-eql nil (test-unique-names)))
+
 #-xlisp-test
 (let ((*print-errors* t)
       (*print-failures* t))
