ad0e82c2d05f4d383af9b7752bf0b7b1d4321527
William Clifford <wobh@yahoo.com>
2016-03-07 22:27:06 -0800

    xlisp #104: renamed `point-mutations` to `hamming`
    - changed the test code package from point-mutation-tests to hamming-test
    - changed the production code file name from dna.lisp to hamming.lisp
    - changed the production code package
    - changed the name of the directory
    - changed entry in config.json

---
+++ exercises/hamming/hamming.lisp
@@ -0,0 +1,9 @@
+(defpackage #:hamming
+  (:use #:cl)
+  (:export #:distance))
+
+(in-package #:hamming)
+
+(defun distance (dna1 dna2)
+  "Number of positional differences in two equal length dna strands."
+  )
--- exercises/point-mutations/example.lisp
+++
@@ -1,9 +0,0 @@
-(defpackage #:dna
-  (:use #:common-lisp)
-  (:export #:hamming-distance))
-
-(in-package #:dna)
-
-(defun hamming-distance (dna1 dna2)
-  (if (= (length dna1) (length dna2))
-      (count nil (map 'list #'char= dna1 dna2))))
