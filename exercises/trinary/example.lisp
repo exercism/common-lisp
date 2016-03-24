bad838cc7b94b0983cdf06fad2281abd4b4eb255
Mark Simpson <verdammelt@gmail.com>
2016-03-23 21:11:03 -0400

    Even easier implementation.

--- exercises/trinary/example.lisp
+++ exercises/trinary/example.lisp
@@ -7,11 +7,19 @@
 (defun to-decimal (string)
   "Return the decimal value for STRING which is in trinary. If STRING
 is not a valid trinary number then return 0."
-  (loop with revstr = (reverse string)
-     for idx below (length string) and c across revstr
-     for digit = (digit-char-p c 3)
-     if (not digit) return 0
-     else summing (* digit (expt 3 idx))))
+  (or (ignore-errors (parse-integer string :radix 3))
+      0))
+
+;;;
+;;; Another possible implementation, if one wanted to reimplement
+;;; PARSE-INTEGER
+;;;
+;; (defun to-decimal (string)
+;;   (loop with revstr = (reverse string)
+;;      for idx below (length string) and c across revstr
+;;      for digit = (digit-char-p c 3)
+;;      if (not digit) return 0
+;;      else summing (* digit (expt 3 idx))))
 
 ;;;
 ;;; Another possible implementation but has a security flaw.
