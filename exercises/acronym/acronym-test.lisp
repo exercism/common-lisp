86ac8458db5e93136e1edface173d1ee98155401
serialhex <serialhex@gmail.com>
2017-10-23 21:32:45 -0400

    added acronym exercise (#187)
    * added acronym exercise
    
    * fixing small things for the PR to succeed

---
+++ exercises/acronym/acronym-test.lisp
@@ -0,0 +1,48 @@
+(ql:quickload "lisp-unit")
+#-xlisp-test (load "acronym")
+
+(defpackage #:acronym-test
+  (:use #:common-lisp #:lisp-unit))
+
+(in-package #:acronym-test)
+
+
+(define-test empty-gives-empty
+  (assert-equal
+    ""
+    (acronym:acronym "")))
+
+(define-test png-test
+  (assert-equal
+    "PNG"
+    (acronym:acronym "Portable Network Graphics")))
+
+(define-test ror-test
+  (assert-equal
+    "ROR"
+    (acronym:acronym "Ruby on Rails")))
+
+(define-test html-test
+  (assert-equal
+    "HTML"
+    (acronym:acronym "HyperText Markup Language")))
+
+(define-test fifo-test
+  (assert-equal
+    "FIFO"
+    (acronym:acronym "First In, First Out")))
+
+(define-test php-test
+  (assert-equal
+    "PHP"
+    (acronym:acronym "PHP: Hypertext Preprocessor")))
+
+(define-test cmos-test
+  (assert-equal
+    "CMOS"
+    (acronym:acronym "Complementary metal-oxide semiconductor")))
+
+#-xlisp-test
+(let ((*print-errors* t)
+      (*print-failures* t))
+  (run-tests :all :acronym-test))
---
+++ exercises/acronym/example.lisp
@@ -0,0 +1,24 @@
+(in-package #:cl-user)
+(defpackage #:acronym
+  (:use #:cl)
+  (:export #:acronym))
+
+(in-package #:acronym)
+
+(defun acronym (str)
+  (labels ((recur (st ls)
+    (cond
+      ((equal "" st)
+        ls)
+      ((both-case-p (elt st 0))
+       (recur (string-left-trim
+              "abcdefghijklmnopqrstuvwxyz"
+              (string-left-trim
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                st))
+             (cons (elt st 0)
+                   ls)))
+      (t (recur (subseq st 1) ls)))))
+    (map 'string
+         #'char-upcase
+         (nreverse (recur str nil)))))
