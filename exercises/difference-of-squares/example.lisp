ca246f406eff0a19691a0cbeced887ed3dfccc5b
Scientifica96 <bentbibrachad@gmail.com>
2018-08-29 13:59:42 +0100

    difference-of-sums: rename square-of-sums to square-of-sum

--- exercises/difference-of-squares/example.lisp
+++ exercises/difference-of-squares/example.lisp
@@ -1,17 +1,17 @@
 (defpackage #:squares
   (:use #:cl)
   (:export #:sum-of-squares
-           #:square-of-sums
+           #:square-of-sum
            #:difference))
 
 (in-package #:squares)
 
-(defun square-of-sums (n)
+(defun square-of-sum (n)
   (expt (* 1/2 n (1+ n)) 2))
 
 (defun sum-of-squares (n)
   (* 1/6 n (1+ n) (1+ (* 2 n))))
 
 (defun difference (n)
-  (- (square-of-sums n)
+  (- (square-of-sum n)
      (sum-of-squares n)))
