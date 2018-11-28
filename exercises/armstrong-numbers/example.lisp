27cdadb01b6060ac51c010c55ea539a2766d7df2
Mark Simpson <verdammelt@gmail.com>
2018-11-27 21:51:02 -0500

    Implement example code.

--- exercises/armstrong-numbers/example.lisp
+++ exercises/armstrong-numbers/example.lisp
@@ -4,5 +4,13 @@
   (:export #:isarmstrongnumber))
 (in-package #:armstrong-numbers)
 
-(defun isarmstrongnumber (number))
+(defun number->digits (number)
+  (map 'list #'digit-char-p (prin1-to-string number)))
 
+(defun sum (numbers) (reduce #'+ numbers))
+
+(defun isarmstrongnumber (number)
+  (let ((digits (number->digits number)))
+    (= number
+       (sum (mapcar #'(lambda (x) (expt x (length digits)))
+                    digits)))))
