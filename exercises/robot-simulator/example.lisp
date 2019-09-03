52574cd62ab74a46963931581b70af520c249360
Wojciech S. Gac <wojciech.s.gac@gmail.com>
2019-09-03 14:44:10 +0200

    Simplify module interface - hide movement details

--- exercises/robot-simulator/example.lisp
+++ exercises/robot-simulator/example.lisp
@@ -1,7 +1,6 @@
 (defpackage #:robot-simulator
   (:use #:common-lisp)
-  (:export #:north #:east #:south #:west
-	   #:turn-right #:turn-left #:advance #:execute-sequence
+  (:export #:north #:east #:south #:west #:execute-sequence
 	   #:robot #:robot-position #:robot-bearing #:make-robot))
 
 (in-package #:robot-simulator)
--- exercises/robot-simulator/robot-simulator.lisp
+++ exercises/robot-simulator/robot-simulator.lisp
@@ -1,8 +1,7 @@
 (in-package #:cl-user)
 (defpackage #:robot-simulator
   (:use #:common-lisp)
-  (:export #:north #:east #:south #:west
-	   #:turn-right #:turn-left #:advance #:execute-sequence
+  (:export #:north #:east #:south #:west #:execute-sequence
 	   #:robot #:robot-position #:robot-bearing #:make-robot))
 
 (in-package #:robot-simulator)
