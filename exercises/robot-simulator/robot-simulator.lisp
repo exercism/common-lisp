2e21114e964a9e7aaf3da2d6bacae33755ec2822
Mark Simpson <verdammelt@gmail.com>
2020-05-01 13:46:44 -0400

    Change constant names to be more idiomatic.

--- exercises/robot-simulator/robot-simulator.lisp
+++ exercises/robot-simulator/robot-simulator.lisp
@@ -1,7 +1,7 @@
 (in-package #:cl-user)
 (defpackage #:robot-simulator
   (:use #:common-lisp)
-  (:export #:north #:east #:south #:west #:execute-sequence
-	   #:robot #:robot-position #:robot-bearing #:make-robot))
+  (:export #:+north+ #:+east+ #:+south+ #:+west+ #:execute-sequence
+           #:robot #:robot-position #:robot-bearing #:make-robot))
 
 (in-package #:robot-simulator)
