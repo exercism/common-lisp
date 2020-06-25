45dad8e1f15fec0076129540d6083a62800a08f0
chuchana <chuchana@gmx.de>
2020-06-25 13:48:08 +0200

    Update luhn-test.lisp

--- exercises/luhn/luhn-test.lisp
+++ exercises/luhn/luhn-test.lisp
@@ -7,46 +7,46 @@
 (in-package #:luhn-test)
 
 (define-test single-digit-strings-can-not-be-valid
-  (assert-false (luhn:valid-p "1")))
+  (assert-false (luhn:validp "1")))
 
 (define-test a-single-zero-is-invalid
-  (assert-false (luhn:valid-p "0")))
+  (assert-false (luhn:validp "0")))
 
 (define-test a-simple-valid-sin-that-remains-valid-if-reversed
-  (assert-true (luhn:valid-p "059")))
+  (assert-true (luhn:validp "059")))
 
 (define-test a-simple-valid-sin-that-becomes-invalid-if-reversed
-  (assert-true (luhn:valid-p "59")))
+  (assert-true (luhn:validp "59")))
 
 (define-test a-valid-canadian-sin
-  (assert-true (luhn:valid-p "055 444 285")))
+  (assert-true (luhn:validp "055 444 285")))
 
 (define-test invalid-canadian-sin
-  (assert-false (luhn:valid-p "055 444 286")))
+  (assert-false (luhn:validp "055 444 286")))
 
 (define-test invalid-credit-card
-  (assert-false (luhn:valid-p "8273 1232 7352 0569")))
+  (assert-false (luhn:validp "8273 1232 7352 0569")))
 
 (define-test valid-strings-with-a-non-digit-included-become-invalid
-  (assert-false (luhn:valid-p "055a 444 285")))
+  (assert-false (luhn:validp "055a 444 285")))
 
 (define-test valid-strings-with-punctuation-included-become-invalid
-  (assert-false (luhn:valid-p "055-444-285")))
+  (assert-false (luhn:validp "055-444-285")))
 
 (define-test valid-strings-with-symbols-included-become-invalid
-  (assert-false (luhn:valid-p "055£ 444$ 285")))
+  (assert-false (luhn:validp "055£ 444$ 285")))
 
 (define-test single-zero-with-space-is-invalid
-  (assert-false (luhn:valid-p " 0")))
+  (assert-false (luhn:validp " 0")))
 
-(define-test more-than-a-single-zero-valid-p
-  (assert-true (luhn:valid-p "0000 0")))
+(define-test more-than-a-single-zero-validp
+  (assert-true (luhn:validp "0000 0")))
 
 (define-test input-digit-9-is-correctly-converted-to-output-digit-9
-  (assert-true (luhn:valid-p "091")))
+  (assert-true (luhn:validp "091")))
 
 (define-test strings-with-non-digits-is-invalid
-  (assert-false (luhn:valid-p ":9")))
+  (assert-false (luhn:validp ":9")))
 
 #-xlisp-test
 (let ((*print-errors* t)
