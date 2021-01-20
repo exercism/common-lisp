8e58d040cf1a643e7982c4a64bfa214cb95406f7
Mark Simpson <verdammelt@gmail.com>
2021-01-19 21:19:18 -0500

    Remove usage of sharpsign-dot in the example source file.
    The V3 representer will not (and can not) handle the use of #..
    
    It is planned to test the representer by representing each exercise in
    the track to ensure it can handle them - thus this example must be
    changed.

--- exercises/space-age/example.lisp
+++ exercises/space-age/example.lisp
@@ -6,22 +6,22 @@
 (defvar +earth-period+ 31557600)
 
 (defvar +planets-and-periods+
-  '((:mercury #.(* 0.2408467 +earth-period+))
-    (:venus #. (* 0.61519726 +earth-period+))
-    (:earth #.(* 1 +earth-period+))
-    (:mars #.(* 1.8808158 +earth-period+))
-    (:jupiter #.(* 11.862615 +earth-period+))
-    (:saturn #.(* 29.447498 +earth-period+))
-    (:uranus #.(* 84.016846 +earth-period+))
-    (:neptune #.(* 164.79132 +earth-period+))))
+  `((:mercury ,(* 0.2408467 +earth-period+))
+    (:venus ,(* 0.61519726 +earth-period+))
+    (:earth ,(* 1 +earth-period+))
+    (:mars ,(* 1.8808158 +earth-period+))
+    (:jupiter ,(* 11.862615 +earth-period+))
+    (:saturn ,(* 29.447498 +earth-period+))
+    (:uranus ,(* 84.016846 +earth-period+))
+    (:neptune ,(* 164.79132 +earth-period+))))
 
 (dolist (planet-info +planets-and-periods+)
   (let ((name (first planet-info))
-	(period (second planet-info)))
+        (period (second planet-info)))
     (let ((symbol (intern (string-upcase
-			   (concatenate 'string
-					"on-"
-					(string name))))))
+                           (concatenate 'string
+                                        "on-"
+                                        (string name))))))
       (setf (symbol-function symbol)
-	    #'(lambda (seconds) (/ seconds period)))
+            #'(lambda (seconds) (/ seconds period)))
       (export symbol))))
