45d01bb7832da3b02cbe59d734d533a74349c59e
Mark Simpson <verdammelt@gmail.com>
2021-01-30 23:22:33 -0500

    Signal error on any non-passing tests.

--- src/test-exercises/main.lisp
+++ src/test-exercises/main.lisp
@@ -3,8 +3,12 @@
 (defun run-all-tests ()
   (let ((all-exercise-directories (append (list-exercise-directories "concept")
                                           (list-exercise-directories "practice"))))
-    (report-results
-     (nreverse
-      (reduce #'(lambda (acc ex) (push (test-exercise ex) acc))
-              all-exercise-directories
-              :initial-value (list))))))
+    (let ((result-totals
+            (report-results
+             (nreverse
+              (reduce #'(lambda (acc ex) (push (test-exercise ex) acc))
+                      all-exercise-directories
+                      :initial-value (list))))))
+      (when (or (plusp (getf result-totals :fail))
+                (plusp (getf result-totals :errors)))
+        (error "Test Failures Detected.")))))
