dc3e282ce11cf07a7ffd52391f7821002d943afe
Mark Simpson <verdammelt@gmail.com>
2021-02-05 23:43:35 -0500

    Tweak CI implementation to handle new practice file.

--- src/test-exercises/exercises.lisp
+++ src/test-exercises/exercises.lisp
@@ -4,21 +4,14 @@
   (let ((config-file (merge-pathnames ".meta/config.json" dir)))
     (if (probe-file config-file)
         (cl-json:decode-json-from-source config-file)
-        ;; this is a v2 exercise not yet converted let's try to do something useful
-        (let ((slug (first (last (pathname-directory dir)))))
-          (pairlis '(:files :v2)
-                   (list (pairlis '(:test :solution :exemplar)
-                                  (list (list (format nil "~A-test.lisp" slug))
-                                        (list (format nil "~A.lisp" slug))
-                                        (list "example.lisp")))
-                         t))))))
+        (error 'file-error :pathname config-file))))
 
 (defun gather-exercise-data (dir)
   (destructuring-bind (type slug)
       (mapcar #'keywordize (last (pathname-directory dir) 2))
     (let ((config (slurp-exercise-config dir)))
       (pairlis '(:directory :type :slug :files :v2)
-               (list dir type slug (aget :files config) (aget :v2 config))))))
+               (list dir type slug (aget :files config) (aget :v-2 config))))))
 
 (defun load-exercise-file (exercise file)
   (let ((*default-pathname-defaults* (aget :directory exercise))
