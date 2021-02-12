95ae134b67f2ee75658c036a2e7f390b201aecd8
Mark Simpson <verdammelt@gmail.com>
2021-02-12 11:33:12 -0500

    Update hello-world practice exercise to v3.

--- exercises/practice/hello-world/hello-world-test.lisp
+++ exercises/practice/hello-world/hello-world-test.lisp
@@ -1,20 +1,27 @@
-;;; 
-;;; hello-world v1.1.0
-;;; 
-(ql:quickload "lisp-unit")
-#-xlisp-test (load "hello-world")
+;; Ensures that key-comparison.lisp and the testing library are always loaded
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (load "hello-world")
+  (ql:quickload :fiveam))
 
-(defpackage #:hello-world-test
-  (:use #:common-lisp #:lisp-unit))
-(in-package #:hello-world-test)
+;; Defines the testing package with FiveAM in scope
+;; The `run-tests` function is exported for use by both the user and test-runner
+(defpackage :hello-world-test
+  (:use :cl :fiveam)
+  (:export :run-tests))
 
-(define-test
-  say-hi!
-  (assert-equal
-    "Hello, World!"
-    (hello-world:hello)))
+;; Enter the testing package
+(in-package :hello-world-test)
 
-#-xlisp-test
-(let ((*print-errors* t)
-      (*print-failures* t))
-  (run-tests :all))
+;; Define and enter a new FiveAM test-suite
+(def-suite hello-world-suite)
+(in-suite hello-world-suite)
+
+(test say-hi
+      (is (string= "Hello, World!" (hello-world:hello))))
+
+;; Either provides human-readable results to the user or machine-readable
+;; results to the test runner. The default upon calling `(run-tests)` is to
+;; explain the results in a human-readable way
+(defun run-tests (&optional (explain t))
+  (let ((tests (run 'hello-world-suite))) ; Run the tests once
+    (if explain (explain! tests) tests))) ; Optionally explain the results
