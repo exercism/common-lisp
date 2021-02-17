33c1af21a7137e52e5258906b153644c95e8d591
Mark Simpson <verdammelt@gmail.com>
2021-02-16 22:58:15 -0500

    Convert hello-world.

--- exercises/practice/hello-world/hello-world-test.lisp
+++ exercises/practice/hello-world/hello-world-test.lisp
@@ -1,20 +1,22 @@
-;;; 
-;;; hello-world v1.1.0
-;;; 
-(ql:quickload "lisp-unit")
-#-xlisp-test (load "hello-world")
+;; Ensures that hello-world.lisp and the testing library are always loaded
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (load "hello-world")
+  (quicklisp-client:quickload :fiveam))
 
+;; Defines the testing package with symbols from hello-world and FiveAM in scope
+;; The `run-tests` function is exported for use by both the user and test-runner
 (defpackage #:hello-world-test
-  (:use #:common-lisp #:lisp-unit))
+  (:use #:cl #:fiveam)
+  (:export #:run-tests))
+
+;; Enter the testing package
 (in-package #:hello-world-test)
 
-(define-test
-  say-hi!
-  (assert-equal
-    "Hello, World!"
-    (hello-world:hello)))
+;; Define and enter a new FiveAM test-suite
+(def-suite* hello-world-suite)
 
-#-xlisp-test
-(let ((*print-errors* t)
-      (*print-failures* t))
-  (run-tests :all))
+(test say-hi! (is (equal "Hello, World!" (hello-world:hello))))
+
+(defun run-tests (&optional (test-or-suite 'hello-world-suite))
+  "Provides human readable results of test run. Default to entire suite."
+  (run! test-or-suite))
