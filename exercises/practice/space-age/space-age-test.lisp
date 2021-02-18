0d5f142af17e113ad1e6c746e133bc61cae0f6ca
Mark Simpson <verdammelt@gmail.com>
2021-02-17 20:35:18 -0500

    Convert space-age. (entirely manual)

--- exercises/practice/space-age/space-age-test.lisp
+++ exercises/practice/space-age/space-age-test.lisp
@@ -1,55 +1,62 @@
-(ql:quickload "lisp-unit")
-#-xlisp-test (load "space-age")
+;; Ensures that space-age.lisp and the testing library are always loaded
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (load "space-age")
+  (quicklisp-client:quickload :fiveam))
 
+;; Defines the testing package with symbols from space-age and FiveAM in scope
+;; The `run-tests` function is exported for use by both the user and test-runner
 (defpackage #:space-age-test
-  (:use #:common-lisp #:lisp-unit))
+  (:use #:cl #:fiveam)
+  (:export #:run-tests))
 
+;; Enter the testing package
 (in-package #:space-age-test)
+
+;; Define and enter a new FiveAM test-suite
+(def-suite* space-age-suite)
 
 (defun rounds-to (expected actual)
   (flet ((to-2-places (n) (/ (fround (* 100 n)))))
-    (assert-equal (to-2-places expected)
-			(to-2-places actual))))
+    (is (= (to-2-places expected) (to-2-places actual)))))
 
-(define-test age-in-earth-years
+(test age-in-earth-years
   (rounds-to 31.69 (space-age:on-earth 1000000001)))
 
-(define-test age-in-mercury-years
+(test age-in-mercury-years
   (let ((seconds 2134835688))
     (rounds-to 67.65 (space-age:on-earth seconds))
     (rounds-to 280.88 (space-age:on-mercury seconds))))
 
-(define-test age-in-venus-years
+(test age-in-venus-years
   (let ((seconds 189839836))
     (rounds-to 6.02 (space-age:on-earth seconds))
     (rounds-to 9.78 (space-age:on-venus seconds))))
 
-(define-test age-on-mars
+(test age-on-mars
   (let ((seconds 2329871239))
     (rounds-to 73.83 (space-age:on-earth seconds))
     (rounds-to 39.25 (space-age:on-mars seconds))))
 
-(define-test age-on-jupiter
+(test age-on-jupiter
   (let ((seconds 901876382))
     (rounds-to 28.58 (space-age:on-earth seconds))
     (rounds-to 2.41 (space-age:on-jupiter seconds))))
 
-(define-test age-on-saturn
+(test age-on-saturn
   (let ((seconds 3000000000))
     (rounds-to 95.06 (space-age:on-earth seconds))
     (rounds-to 3.23 (space-age:on-saturn seconds))))
 
-(define-test age-on-uranus
+(test age-on-uranus
   (let ((seconds 3210123456))
     (rounds-to 101.72 (space-age:on-earth seconds))
     (rounds-to 1.21 (space-age:on-uranus seconds))))
 
-(define-test age-on-neptune
+(test age-on-neptune
   (let ((seconds 8210123456))
     (rounds-to 260.16 (space-age:on-earth seconds))
     (rounds-to 1.58 (space-age:on-neptune seconds))))
 
-#-xlisp-test
-(let ((*print-errors* t)
-      (*print-failures* t))
-  (run-tests :all :space-age-test))
+(defun run-tests (&optional (test-or-suite 'space-age-suite))
+  "Provides human readable results of test run. Default to entire suite."
+  (run! test-or-suite))
--- exercises/practice/space-age/space-age.lisp
+++ exercises/practice/space-age/space-age.lisp
@@ -1,6 +1,14 @@
 (in-package #:cl-user)
 (defpackage #:space-age
-  (:use #:common-lisp))
+  (:use #:cl)
+  (:export #:on-mercury
+           #:on-venus
+           #:on-earth
+           #:on-mars
+           #:on-jupiter
+           #:on-saturn
+           #:on-uranus
+           #:on-neptune))
 
 (in-package #:space-age)
 
