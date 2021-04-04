5107d36684a8a79055aba2a340774a3b3531c8b6
Mark Simpson <verdammelt@gmail.com>
2021-04-04 17:12:15 -0400

    Add checker to ensure concepts from exercises are in the list.
    Concept exercises list concepts directly, practice exercises have both
    "prerequisites" and "practices" concepts. All of these should appear
    in the concepts list.

--- src/config-checker/main.lisp
+++ src/config-checker/main.lisp
@@ -11,7 +11,31 @@
         (format *error-output* "Concept: ~S does not have a directory~&" c))
       (error 'config-check-failure))))
 
-(defparameter *checkers* (list #'each-concept-has-a-directory))
+(defun exercise-concepts-are-in-concept-list (config)
+  (let* ((exercises (gethash "exercises" config))
+         (concepts (sort (copy-seq (mapcar #'(lambda (h) (gethash "slug" h))
+                                           (gethash "concepts" config)))
+                         #'string<))
+         (concept-exercises  (gethash "concept" exercises))
+         (practice-exercises (gethash "practice" exercises))
+
+         (exercise-concepts
+           (delete-duplicates
+            (sort
+             (append (apply #'append (mapcar #'(lambda (h) (gethash "concepts" h)) concept-exercises))
+                     (apply #'append (mapcar #'(lambda (h) (gethash "practices" h)) practice-exercises))
+                     (apply #'append (mapcar #'(lambda (h) (gethash "prerequisites" h)) practice-exercises)))
+             #'string<)
+            :test #'string=)))
+
+    (let ((unlisted-concepts (set-difference exercise-concepts concepts :test #'string=)))
+      (when unlisted-concepts
+        (format *error-output* "Concepts from exercises: ~S not in the concept list~&"
+                unlisted-concepts)
+        (error 'config-check-failure)))))
+
+(defparameter *checkers* (list #'each-concept-has-a-directory
+                               #'exercise-concepts-are-in-concept-list))
 
 (defun check-config ()
   (let ((config (yason:parse (truename "./config.json"))))
