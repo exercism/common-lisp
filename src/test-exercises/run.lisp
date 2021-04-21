49b808e653ccf27b2b4cd92a3577980e0dd2004e
Mark Simpson <verdammelt@gmail.com>
2021-04-20 21:23:40 -0400

    Remove traces of v2 test runner from CI - no longer needed.

--- src/test-exercises/run.lisp
+++ src/test-exercises/run.lisp
@@ -1,27 +1,10 @@
 (in-package #:test-exercises)
 
-;; for v2 test running
-(pushnew :xlisp-test *features*)
-
 (defun example-files (data)
   (or (aget :exemplar (aget :files data))
       (aget :example (aget :files data))))
 
-(defun test-v2-exercise (data)
-  (let ((test-files (aget :test (aget :files data)))
-        (solution-files (aget :solution (aget :files data)))
-        (exemplar-files (example-files data)))
-    (unwind-protect
-         (progn
-           (dolist (f (append solution-files exemplar-files test-files))
-             (load-exercise-file data f))
-           (list
-            (let ((lisp-unit:*summarize-results* nil))
-              (lisp-unit:run-tests :all (find-exercise-package data :test t)))))
-      (delete-package (find-exercise-package data :test t))
-      (delete-package (find-exercise-package data)))))
-
-(defun test-v3-exercise (data)
+(defun %test-exercise (data)
   (let ((test-files (aget :test (aget :files data)))
         (exemplar-files (example-files data)))
     (unwind-protect
@@ -41,7 +24,5 @@
     (pairlis '(:exercise :results)
              (list data
                    (handler-case
-                       (if (aget :v2 data)
-                           (test-v2-exercise data)
-                           (test-v3-exercise data))
+                       (%test-exercise data)
                      (error (c) (list c)))))))
