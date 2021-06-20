df5b3a825fcd31f9c63a79bade52525e44cceabe
Mark Simpson <verdammelt@gmail.com>
2021-06-20 19:29:59 -0400

    Adding tests, stub and exemplar.

--- exercises/concept/character-study/character-study.lisp
+++ exercises/concept/character-study/character-study.lisp
@@ -1,4 +1,16 @@
 (defpackage :character-study
   (:use :cl)
-  (:export))
+  (:export
+   :compare-chars
+   :size-of-char
+   :change-size-of-char
+   :type-of-char))
 (in-package :character-study)
+
+(defun compare-chars (char1 char2))
+
+(defun size-of-char (char))
+
+(defun change-size-of-char (char wanted-size))
+
+(defun type-of-char (char))
