f3f92514543fe054ead71d9017b01477ce3ecf42
mtreis86 <michael@mtreis86.com>
2021-09-07 16:05:32 -0400

    Update robot-simulator.lisp
    Functions required for tests didn't exist

--- exercises/practice/robot-simulator/robot-simulator.lisp
+++ exercises/practice/robot-simulator/robot-simulator.lisp
@@ -4,3 +4,16 @@
            :robot :robot-position :robot-bearing :make-robot))
 
 (in-package :robot-simulator)
+
+(defun make-robot (&key position bearing))
+  "A robot has a position on a grid made up of a cons of positive numbers,
+  and a bearing of +north+ +south+ +east+ or +west+"
+ 
+(defun execute-sequence (robot sequence))
+  "Moves and rotates the robot according to the sequence"
+
+(defun robot-position (robot))
+  "Returns the robot's position"
+
+(defun robot-bearing (robot))
+  "Returns the robot's bearing"
