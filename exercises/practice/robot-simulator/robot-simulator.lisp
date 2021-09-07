10e048e32dc1c72d5442b144c834ef19a8efc019
mtreis86 <michael@mtreis86.com>
2021-09-07 16:21:02 -0400

    Update robot-simulator.lisp
    Default functions didn't exist

--- exercises/practice/robot-simulator/robot-simulator.lisp
+++ exercises/practice/robot-simulator/robot-simulator.lisp
@@ -4,3 +4,11 @@
            :robot :robot-position :robot-bearing :make-robot))
 
 (in-package :robot-simulator)
+
+;; A robot has a position on a grid made up of a cons of positive numbers,
+;; and a bearing of +north+ +south+ +east+ or +west+
+(defstruct robot
+  position bearing)
+
+(defun execute-sequence (robot sequence)
+  "Moves and rotates the robot according to the sequence")
