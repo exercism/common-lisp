34549ca3bd272062593321879ec11bb4c0622663
Mark Simpson <verdammelt@gmail.com>
2021-09-08 13:58:16 -0400

    Update isogram exercise.
    Both from problem-specifications and choosing a more lispy predicate name.

--- exercises/practice/isogram/isogram-test.lisp
+++ exercises/practice/isogram/isogram-test.lisp
@@ -15,30 +15,45 @@
 ;; Define and enter a new FiveAM test-suite
 (def-suite* isogram-suite)
 
-(test empty-string (is (isogram:is-isogram "")))
+(test empty-string (is (isogram:isogram-p "")))
 
 (test isogram-with-only-lower-case-characters
- (is (isogram:is-isogram "isogram")))
+  (is (isogram:isogram-p "isogram")))
 
 (test word-with-one-duplicated-character
- (is (not (isogram:is-isogram "eleven"))))
+  (is (not (isogram:isogram-p "eleven"))))
+
+(test word-with-one-duplicated-character-from-the-end-of-the-alphabet
+  (is (not (isogram:isogram-p "zzyzx"))))
 
 (test longest-reported-english-isogram
- (is (isogram:is-isogram "subdermatoglyphic")))
+  (is (isogram:isogram-p "subdermatoglyphic")))
 
 (test word-with-duplicated-character-in-mixed-case
- (is (not (isogram:is-isogram "Alphabet"))))
+  (is (not (isogram:isogram-p "Alphabet"))))
+
+(test word-with-duplicated-character-in-mixed-case-lowercase-first
+  (is (not (isogram:isogram-p "alphAbet"))))
 
 (test hypothetical-isogrammic-word-with-hyphen
- (is (isogram:is-isogram "thumbscrew-japingly")))
+  (is (isogram:isogram-p "thumbscrew-japingly")))
 
-(test isogram-with-duplicated-hyphen (is (isogram:is-isogram "six-year-old")))
+(test hypothetical-word-with-duplicated-character-following-hyphen
+  (is (not (isogram:isogram-p "thumbscrew-jappingly"))))
+
+(test isogram-with-duplicated-hyphen (is (isogram:isogram-p "six-year-old")))
 
 (test made-up-name-that-is-an-isogam
- (is (isogram:is-isogram "Emily Jung Schwartzkopf")))
+  (is (isogram:isogram-p "Emily Jung Schwartzkopf")))
 
 (test duplicated-character-in-the-middle
- (is (not (isogram:is-isogram "accentor"))))
+  (is (not (isogram:isogram-p "accentor"))))
+
+(test same-first-and-last-characters
+  (is (not (isogram:isogram-p "angola"))))
+
+(test word-with-duplicated-character-and-with-two-hyphens
+  (is (not (isogram:isogram-p "up-to-date"))))
 
 (defun run-tests (&optional (test-or-suite 'isogram-suite))
   "Provides human readable results of test run. Default to entire suite."
