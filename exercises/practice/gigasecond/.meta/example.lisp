ff5343183093fca316895c345a234cb12bc756ea
Mark Simpson <verdammelt@gmail.com>
2021-09-14 20:39:00 -0400

    Re-add gigasecond practice exercise
    Appears to be required by the syncing.

---
+++ exercises/practice/gigasecond/.meta/example.lisp
@@ -0,0 +1,16 @@
+(defpackage #:gigasecond
+  (:use #:cl)
+  (:export #:from))
+
+(in-package #:gigasecond)
+
+(defun from (year month day hour minute second)
+  (reverse
+   (subseq
+    (multiple-value-list
+     (let ((TZ 0))
+       (decode-universal-time
+        (+ (encode-universal-time second minute hour day month year TZ)
+           (expt 10 9))
+        TZ)))
+    0 6)))
