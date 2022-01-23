0be053e7becc46216fe6006ca60f3a7b59ecd95c
Mark Simpson <verdammelt@gmail.com>
2022-01-23 15:58:33 -0500

    Update luhn exercise (#558)

--- exercises/practice/luhn/luhn-test.lisp
+++ exercises/practice/luhn/luhn-test.lisp
@@ -31,23 +31,45 @@
 
 (test invalid-credit-card (is (not (luhn:validp "8273 1232 7352 0569"))))
 
-(test valid-strings-with-a-non-digit-included-become-invalid
- (is (not (luhn:validp "055a 444 285"))))
+(test invalid-long-number-with-even-remainder
+  (is (not (luhn:validp "1 2345 6789 1234 5678 9012"))))
+
+(test valid-number-with-even-number-of-digits
+  (is (luhn:validp "095 245 88")))
+
+(test valid-number-with-odd-number-of-spaces
+  (is (luhn:validp "234 567 891 234")))
+
+(test valid-strings-with-a-non-digit-added-at-the-end-become-invalid
+ (is (not (luhn:validp "059a"))))
 
 (test valid-strings-with-punctuation-included-become-invalid
  (is (not (luhn:validp "055-444-285"))))
 
 (test valid-strings-with-symbols-included-become-invalid
- (is (not (luhn:validp "055£ 444$ 285"))))
+ (is (not (luhn:validp "055# 444$ 285"))))
 
 (test single-zero-with-space-is-invalid (is (not (luhn:validp " 0"))))
 
 (test more-than-a-single-zero-validp (is (luhn:validp "0000 0")))
 
 (test input-digit-9-is-correctly-converted-to-output-digit-9
- (is (luhn:validp "091")))
+  (is (luhn:validp "091")))
 
-(test strings-with-non-digits-is-invalid (is (not (luhn:validp ":9"))))
+(test very-long-input-is-valid
+  (is (luhn:validp "9999999999 9999999999 9999999999 9999999999")))
+
+(test valid-luhn-with-an-odd-number-of-digits-and-non-zero-first-digit
+  (is (luhn:validp "109")))
+
+(test using-ascii-value-for-non-doubled-non-digit-is-not-allowed
+  (is (not (luhn:validp "055b 444 285"))))
+
+(test using-ascii-value-for-doubled-non-digit-is-not-allowed
+  (is (not (luhn:validp ":9"))))
+
+(test non-numeric-non-space-char-in-the-middle-with-a-sum-that's-divisible-by-10-is-not-allowed
+  (is (not (luhn:validp "59%59"))))
 
 (defun run-tests (&optional (test-or-suite 'luhn-suite))
   "Provides human readable results of test run. Default to entire suite."
