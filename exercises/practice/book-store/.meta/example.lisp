51f81a481da4606336477f5bb681f317a9c337ea
PaulT89 <paul.d.turner.1989@gmail.com>
2022-04-24 00:49:37 +1000

    Comments -> Docstrings

--- exercises/practice/book-store/.meta/example.lisp
+++ exercises/practice/book-store/.meta/example.lisp
@@ -12,19 +12,19 @@
     (apply '+ (mapcar #'calculate-group-price groupings)))
    0))
 
-;;; Function to recursively find the largest groups possible from the basket.
-;;; Returns the length of each group of books.
 (defun create-groups (basket)
+ "Function to recursively find the largest groups possible from the basket.
+Returns the length of each group of books."
   (unless (zerop (list-length basket))
     (let ((group (remove-duplicates basket)))
       (loop for book in group
         do (setf basket (remove book basket :count 1)))
       (cons (list-length group) (create-groups basket)))))
 
-;;; Function to replace every pair of lengths 3 & 5 with pairs of length 4 & 4.
-;;; This is required to correctly calculate the price, as pairs of 4 & 4 are
-;;; less expensive than pairs of 5 & 3.
 (defun improve-groupings (groupings)
+  "Function to replace every pair of lengths 3 & 5 with pairs of length 4 & 4.
+This is required to correctly calculate the price, as pairs of 4 & 4 are less
+expensive than pairs of 5 & 3."
  (if (subsetp '(3 5) groupings)
    (improve-groupings (append '(4 4) (remove 5 (remove 3 groupings :count 1) :count 1)))
    groupings))
