c508680cea1c59508e51de97ae964d2bb5a799fa
PaulT89 <paul.d.turner.1989@gmail.com>
2022-05-08 00:52:16 +1000

    Add exercise Run Length Encoding
    Add exercise (generated from problem specifications).

---
+++ exercises/practice/run-length-encoding/run-length-encoding.lisp
@@ -0,0 +1,10 @@
+(defpackage :run-length-encoding
+  (:use :cl)
+  (:export :encode
+           :decode))
+
+(in-package :run-length-encoding)
+
+(defun encode (plain))
+
+(defun decode (compressed))
