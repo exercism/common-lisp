bcab2d722609586553d5c5cb4a4cfc7531c7cdec
PaulT89 <paul.d.turner.1989@gmail.com>
2022-05-09 20:46:33 +1000

    Improved readability of example

--- exercises/practice/matching-brackets/.meta/example.lisp
+++ exercises/practice/matching-brackets/.meta/example.lisp
@@ -11,7 +11,10 @@
 (defun eliminate-pairs (tracker bracket)
   (cond
     ((not tracker) (cons bracket tracker))
-    ((or (and (char= #\( (car tracker)) (char= #\) bracket))
-         (and (char= #\{ (car tracker)) (char= #\} bracket))
-         (and (char= #\[ (car tracker)) (char= #\] bracket))) (cdr tracker))
+    ((matching-pair-p (car tracker) bracket) (cdr tracker))
     (t (cons bracket tracker))))
+
+(defun matching-pair-p (left-bracket right-bracket)
+  (or (and (char= #\( left-bracket) (char= #\) right-bracket))
+      (and (char= #\{ left-bracket) (char= #\} right-bracket))
+      (and (char= #\[ left-bracket) (char= #\] right-bracket))))
