0ff5bf5f82b7df0ce661712cce2071d44f33f58c
PaulT89 <paul.d.turner.1989@gmail.com>
2022-05-10 19:32:41 +1000

    Adding exercise largest-series-product
    Generated practice exercise Largest Series Product.  Not sure if docstrings or comments are needed for certain functions, but happy to receive input on that.
    
    Difficulty = 4 (very high easy).  High end of what other languages rate this as, though since the Lisp solution requires a little bit more work, the difficulty could conceivably be bumped up to a 5 (low medium).

---
+++ exercises/practice/largest-series-product/.meta/example.lisp
@@ -0,0 +1,24 @@
+(defpackage :largest-series-product
+  (:use :cl)
+  (:export :largest-product))
+
+(in-package :largest-series-product)
+
+(defun largest-product (digits span)
+ (when (valid-input-p digits span)
+  (if (zerop span)
+   1
+   (apply #'max (mapcar #'string-to-product (windows digits span))))))
+
+(defun windows (seq span)
+  (loop for start from 0 to (- (length seq) span)
+        for end from span to (length seq)
+    collect (subseq seq start end)))
+
+(defun string-to-product (seq)
+  (apply #'* (mapcar #'digit-char-p (coerce seq 'list))))
+
+(defun valid-input-p (digits span)
+  (and (every #'digit-char-p digits)
+       (not (minusp span))
+       (>= (length digits) span)))
