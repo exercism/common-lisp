08d978048914fb165b80f56b53fc20c11e707e57
PaulT89 <paul.d.turner.1989@gmail.com>
2022-05-17 04:41:56 +1000

    Create exercise protein-translation
    Generated practice exercise Protein Translation.
    
    Difficulty = 5.  Relatively simple exercise with multiple ways of completing it.  The reason that the difficulty is a 5 (and not a 3 or a 4) is that that the student will need to find out about error signalling (like in the Nucleotide Count exercise).

---
+++ exercises/practice/protein-translation/.meta/example.lisp
@@ -0,0 +1,27 @@
+(defpackage :protein-translation
+  (:use :cl)
+  (:export :proteins
+           :invalid-protein))
+
+(in-package :protein-translation)
+
+(define-condition invalid-protein (error) ())
+
+(defun proteins (strand)
+  (let* ((len (length strand))
+         (cut-at (if (>= len 3) 3 len)))
+    (when (and (plusp len)
+               (setf codon (translate (subseq strand 0 cut-at))))
+      (cons codon (proteins (subseq strand 3))))))
+
+(defun translate (protein)
+  (case (intern protein :keyword)
+    (:AUG "Methionine")
+    ((:UUU :UUC) "Phenylalanine")
+    ((:UUA :UUG) "Leucine")
+    ((:UCU :UCC :UCA :UCG) "Serine")
+    ((:UAU :UAC) "Tyrosine")
+    ((:UGU :UGC) "Cysteine")
+    (:UGG "Tryptophan")
+    ((:UAA :UAG :UGA) nil)
+    (otherwise (error 'invalid-protein))))
