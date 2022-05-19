fda8f6060cc191404c16296ac7b5689335bcc25b
PaulT89 <paul.d.turner.1989@gmail.com>
2022-05-19 16:28:41 +1000

    Update example.lisp

--- exercises/practice/largest-series-product/.meta/example.lisp
+++ exercises/practice/largest-series-product/.meta/example.lisp
@@ -4,16 +4,9 @@
 
 (in-package :largest-series-product)
 
-(defun largest-product (digits span)
- (when (valid-input-p digits span)
-  (if (zerop span)
-   1
-   (apply #'max (mapcar #'string-to-product (windows digits span))))))
-
 (defun windows (seq span)
   (loop for start from 0 to (- (length seq) span)
-        for end from span to (length seq)
-    collect (subseq seq start end)))
+    collect (subseq seq start (+ start span))))
 
 (defun string-to-product (seq)
   (apply #'* (map 'list #'digit-char-p seq)))
@@ -22,3 +15,9 @@
   (and (every #'digit-char-p digits)
        (not (minusp span))
        (>= (length digits) span)))
+
+(defun largest-product (digits span)
+  (when (valid-input-p digits span)
+    (if (zerop span)
+      1
+      (apply #'max (mapcar #'string-to-product (windows digits span))))))
