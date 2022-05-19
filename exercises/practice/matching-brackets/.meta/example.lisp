80223b6d04478961ff8208e01677fd1e3059d1a8
PaulT89 <paul.d.turner.1989@gmail.com>
2022-05-19 16:14:06 +1000

    Refactored example
    Made a lookup table for the brackets.  Eliminated the need for matching-pair-p function.  Refactored cond into an if.  Declared/defined eliminate-pairs function before it's called.

--- exercises/practice/matching-brackets/.meta/example.lisp
+++ exercises/practice/matching-brackets/.meta/example.lisp
@@ -4,17 +4,14 @@
 
 (in-package :matching-brackets)
 
+(defparameter +bracket-lookup+ '((#\( . #\)) (#\{ . #\}) (#\[ . #\])))
+
+(defun eliminate-pairs (tracker bracket)
+  (let ((match (rassoc bracket +bracket-lookup+)))
+    (if (and tracker match (char= (car match) (car tracker)))
+      (cdr tracker)
+      (cons bracket tracker))))
+
 (defun pairedp (value)
   (let ((only-brackets (remove-if-not (lambda (c) (find c "{}[]()")) value)))
     (zerop (length (reduce #'eliminate-pairs only-brackets :initial-value '())))))
-
-(defun eliminate-pairs (tracker bracket)
-  (cond
-    ((not tracker) (cons bracket tracker))
-    ((matching-pair-p (car tracker) bracket) (cdr tracker))
-    (t (cons bracket tracker))))
-
-(defun matching-pair-p (left-bracket right-bracket)
-  (or (and (char= #\( left-bracket) (char= #\) right-bracket))
-      (and (char= #\{ left-bracket) (char= #\} right-bracket))
-      (and (char= #\[ left-bracket) (char= #\] right-bracket))))
