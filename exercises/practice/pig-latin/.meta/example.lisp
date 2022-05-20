7eec7b018b7f2852143b565fd4941e5408bea22d
PaulT89 <paul.d.turner.1989@gmail.com>
2022-05-20 13:56:26 +1000

    Refactored example.lisp
    Moved functions around to eliminate warnings on load.  Changed stopp to stop-p.  Made stop-p its own function.

--- exercises/practice/pig-latin/.meta/example.lisp
+++ exercises/practice/pig-latin/.meta/example.lisp
@@ -6,23 +6,6 @@
 
 (defparameter +vowels+ '("a" "e" "i" "o" "u" "xr" "yt"))
 
-(defun translate (phrase)
-  (loop for word in (split-string phrase)
-    collect (translate-word word) into output
-    finally (return (format nil "~{~A~^ ~}" output))))
-
-(defun translate-word (word)
-  (let ((index (take-consonants word)))
-    (format nil "~A~Aay" (subseq word index) (subseq word 0 index))))
-
-(defun take-consonants (word &optional (index 0))
-  (let ((stopp (lambda (w i) (or (starts-with-p w +vowels+)
-                                 (and (char= #\y (char w 0)) (= 1 i))))))
-    (cond
-      ((funcall stopp word index) index)
-      ((starts-with-p word '("qu")) (take-consonants (subseq word 2) (+ 2 index)))
-      (t (take-consonants (subseq word 1) (1+ index))))))
-
 (defun split-string (str)
   (unless (zerop (length str))
     (let* ((split-at (position #\space str))
@@ -35,3 +18,21 @@
     (case (search (car search-list) word)
       (0 T)
       (otherwise (starts-with-p word (cdr search-list))))))
+
+(defun stop-p (word index)
+  (or (starts-with-p word +vowels+) (and (char= #\y (char word 0)) (= 1 index))))
+
+(defun take-consonants (word &optional (index 0))
+  (cond
+    ((stop-p word index) index)
+    ((starts-with-p word '("qu")) (take-consonants (subseq word 2) (+ 2 index)))
+    (t (take-consonants (subseq word 1) (1+ index)))))
+
+(defun translate-word (word)
+  (let ((index (take-consonants word)))
+    (format nil "~A~Aay" (subseq word index) (subseq word 0 index))))
+
+(defun translate (phrase)
+  (loop for word in (split-string phrase)
+    collect (translate-word word) into output
+    finally (return (format nil "~{~A~^ ~}" output))))
