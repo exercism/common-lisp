7214c1cf61008d7963549e83d3d0ac3385a12e54
PaulT89 <paul.d.turner.1989@gmail.com>
2022-05-20 22:59:10 +1000

    Rearranged function to avoid warnings

--- exercises/practice/nth-prime/.meta/example.lisp
+++ exercises/practice/nth-prime/.meta/example.lisp
@@ -4,6 +4,10 @@
 
 (in-package :nth-prime)
 
+(defun not-prime-p (number)
+  (loop for x from 3 to (isqrt number)
+    when (zerop (mod number x)) return t))
+
 (defun find-prime (number)
   (when (plusp number)
     (if (= number 1)
@@ -11,7 +15,3 @@
       (loop for x from 3 by 2 with primes = 1
         unless (not-prime-p x) do (incf primes)
         when (= number primes) return x))))
-
-(defun not-prime-p (number)
-  (loop for x from 3 to (isqrt number)
-    when (zerop (mod number x)) return t))
