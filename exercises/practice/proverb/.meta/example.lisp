42ac6fea25d96787785e51be9dc7bff07049b0b3
PaulT89 <paul.d.turner.1989@gmail.com>
2022-05-26 19:47:04 +1000

    Rewrote test and example
    Test now expects purely a string output, as opposed to before, when it expected a list of strings as output.

--- exercises/practice/proverb/.meta/example.lisp
+++ exercises/practice/proverb/.meta/example.lisp
@@ -5,7 +5,9 @@
 (in-package :proverb)
 
 (defun recite (strings)
-  (when strings
-    (loop for x in (butlast strings) for y in (cdr strings)
-      collect (format nil "For want of a ~A the ~A was lost." x y) into output
-      finally (return (append output (list (format nil "And all for the want of a ~A." (car strings))))))))
+  (if strings
+    (let* ((interleaved (apply #'append (mapcar #'list strings strings)))
+           (without-ends (cdr (butlast interleaved)))
+           (proverb-bulk (format nil "~{For want of a ~A the ~A was lost.~%~}" without-ends)))
+      (format nil "~AAnd all for the want of a ~A." proverb-bulk (car strings)))
+    ""))
--- exercises/practice/proverb/proverb-test.lisp
+++ exercises/practice/proverb/proverb-test.lisp
@@ -17,44 +17,44 @@
 
 (test zero-pieces
     (let ((strings '()))
-      (is (equal '() (proverb:recite strings)))))
+      (is (string= "" (proverb:recite strings)))))
 
 (test one-piece
     (let ((strings '("nail"))
-          (result '("And all for the want of a nail.")))
-      (is (equal result (proverb:recite strings)))))
+          (result "And all for the want of a nail."))
+      (is (string= result (proverb:recite strings)))))
 
 (test two-pieces
     (let ((strings '("nail" "shoe"))
-          (result '("For want of a nail the shoe was lost."
-                    "And all for the want of a nail.")))
-      (is (equal result (proverb:recite strings)))))
+          (result (format nil "~{~A~^~%~}" '("For want of a nail the shoe was lost."
+                                             "And all for the want of a nail."))))
+      (is (string= result (proverb:recite strings)))))
 
 (test three-pieces
     (let ((strings '("nail" "shoe" "horse"))
-          (result '("For want of a nail the shoe was lost."
-                    "For want of a shoe the horse was lost."
-                    "And all for the want of a nail.")))
-      (is (equal result (proverb:recite strings)))))
+          (result (format nil "~{~A~^~%~}" '("For want of a nail the shoe was lost."
+                                             "For want of a shoe the horse was lost."
+                                             "And all for the want of a nail."))))
+      (is (string= result (proverb:recite strings)))))
 
 (test full-proverb
     (let ((strings '("nail" "shoe" "horse" "rider" "message" "battle" "kingdom"))
-          (result '("For want of a nail the shoe was lost."
-                    "For want of a shoe the horse was lost."
-                    "For want of a horse the rider was lost."
-                    "For want of a rider the message was lost."
-                    "For want of a message the battle was lost."
-                    "For want of a battle the kingdom was lost."
-                    "And all for the want of a nail.")))
-      (is (equal result (proverb:recite strings)))))
+          (result (format nil "~{~A~^~%~}" '("For want of a nail the shoe was lost."
+                                             "For want of a shoe the horse was lost."
+                                             "For want of a horse the rider was lost."
+                                             "For want of a rider the message was lost."
+                                             "For want of a message the battle was lost."
+                                             "For want of a battle the kingdom was lost."
+                                             "And all for the want of a nail."))))
+      (is (string= result (proverb:recite strings)))))
 
 (test four-pieces-modernized
     (let ((strings '("pin" "gun" "soldier" "battle"))
-          (result '("For want of a pin the gun was lost."
-                    "For want of a gun the soldier was lost."
-                    "For want of a soldier the battle was lost."
-                    "And all for the want of a pin.")))
-      (is (equal result (proverb:recite strings)))))
+          (result (format nil "~{~A~^~%~}" '("For want of a pin the gun was lost."
+                                             "For want of a gun the soldier was lost."
+                                             "For want of a soldier the battle was lost."
+                                             "And all for the want of a pin."))))
+      (is (string= result (proverb:recite strings)))))
 
 (defun run-tests (&optional (test-or-suite 'proverb-suite))
   "Provides human readable results of test run. Default to entire suite."
