f3f6981830413164ca8ba64e908e0533419c346a
PaulT89 <97411029+PaulT89@users.noreply.github.com>
2022-05-28 19:10:38 +1000

    Rewrote example.lisp
    Uses reduce and recursion instead of recursion only.

--- exercises/practice/flatten-array/.meta/example.lisp
+++ exercises/practice/flatten-array/.meta/example.lisp
@@ -4,14 +4,8 @@
 
 (in-package :flatten-array)
 
-(defun make-flat (nested flattened-list)
-  (if (endp nested)
-    flattened-list
-    (let ((item (car nested))
-          (remains (cdr nested)))
-      (if (atom item)
-        (cons item (make-flat remains flattened-list))
-        (make-flat item (make-flat remains flattened-list))))))
-
 (defun flatten (nested)
-  (remove-if #'null (make-flat nested '())))
+  (remove nil
+          (reduce #'(lambda (acc item)
+                      (append acc (if (atom item) (list item) (flatten item))))
+                  nested :initial-value (list))))
