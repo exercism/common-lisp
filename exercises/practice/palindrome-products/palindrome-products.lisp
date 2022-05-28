43b3e435c4d7c1f60300b625fc45934f9646f6f7
PaulT89 <paul.d.turner.1989@gmail.com>
2022-05-28 23:40:51 +1000

    Create exercise palindrome-products
    Generated practice exercise Palindrome Products.
    
    Difficulty = 6.  This exercise may be somewhat restrictive in how it needs to be done, as (insufficient) speed and memory may be an issue.

---
+++ exercises/practice/palindrome-products/palindrome-products.lisp
@@ -0,0 +1,10 @@
+(defpackage :palindrome-products
+  (:use :cl)
+  (:export :smallest
+           :largest))
+
+(in-package :palindrome-products)
+
+(defun smallest (min-factor max-factor))
+
+(defun largest (min-factor max-factor))
