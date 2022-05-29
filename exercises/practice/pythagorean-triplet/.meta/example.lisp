a1cd0a081e2669dfc0bb6f4fe24ebc181ebf5a07
PaulT89 <paul.d.turner.1989@gmail.com>
2022-05-29 21:25:53 +1000

    Rewrote example.lisp
    Put all steps into the one loop, and wrote a detailed explanation of the math  (and thus the algorithm) at the beginning.

--- exercises/practice/pythagorean-triplet/.meta/example.lisp
+++ exercises/practice/pythagorean-triplet/.meta/example.lisp
@@ -1,15 +1,54 @@
+#|
+For each side a, so long as a <= n / 3.414, the math works like this:
+
+a^2 + b^2 = c^2
+a + b + c = n
+
+where a is the shortest side
+      b is the mid-length side
+      c is the hypotenuse
+      n is the perimeter
+
+b^2 = a^2 - c^2
+b = n - a - c
+
+b^2 = a^2 - c^2
+b^2 = n^2 - 2an + a^2 - 2nc + c^2 + 2ac
+
+a^2 - c^2 = n^2 - 2an + a^2 - 2nc + c^2 + 2ac
+
+n^2 - 2an + a^2 + a^2 - 2nc + 2ac = 0
+
+(n - a)^2 + a^2 - 2c(n - a) = 0
+
+2c(n - a) = a^2 + (n - a)^2
+
+Therefore c = (a^2 + (n - a)^2) / 2(n - a)
+
+But we also want c to be a a whole number
+
+Therefore numerator = a^2 + (n - a)^2
+          denominator = 2(n - a)
+
+If numerator % denominator == 0 then c is a whole number, else go to next a
+
+Given that c is a whole number, and we know a and n, we can calculate b:
+
+a + b + c = n
+
+Therefore b = n - a - c
+|#
+
 (defpackage :pythagorean-triplet
   (:use :cl)
   (:export :triplets-with-sum))
 
 (in-package :pythagorean-triplet)
 
-(defun work-triplet (triplet n)
-  (let ((third-on-second (/ (third triplet) (second triplet))))
-    (list (first triplet) (- n (first triplet) third-on-second) third-on-second)))
+(defparameter +right-isoceles-perimeter-ratio+ 3.414) ; approx 1 + 1 + 2^(1/2)
 
 (defun triplets-with-sum (n)
-  (let* ((unworked-triplets (loop for a from 1 to (floor (* 2 n) 7)
-                              collect (list a (* 2 (- n a)) (+ (expt a 2) (expt (- n a) 2)))))
-         (filtered (remove-if (lambda (x) (plusp (mod (third x) (second x)))) unworked-triplets)))
-    (mapcar (lambda (x) (work-triplet x n)) filtered)))
+  (loop for a from 1 to (floor n +right-isoceles-perimeter-ratio+) with c and modulo
+    do (multiple-value-setq (c modulo) (floor (+ (expt a 2) (expt (- n a) 2)) (* 2 (- n a))))
+    when (zerop modulo)
+    collect (list a (- n a c) c)))
