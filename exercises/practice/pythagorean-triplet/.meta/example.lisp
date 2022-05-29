c39637edf01757f2196e4826608c84b7e842e359
PaulT89 <paul.d.turner.1989@gmail.com>
2022-05-29 22:17:52 +1000

    Updated explanation in example.lisp
    Fixed math mistakes, made sections easier to read.

--- exercises/practice/pythagorean-triplet/.meta/example.lisp
+++ exercises/practice/pythagorean-triplet/.meta/example.lisp
@@ -1,5 +1,5 @@
 #|
-For each side a, so long as a <= n / 3.414, the math works like this:
+For each side a, so long as 0 < a < n / 3.414, the math works like this:
 
 a^2 + b^2 = c^2
 a + b + c = n
@@ -9,15 +9,15 @@ where a is the shortest side
       c is the hypotenuse
       n is the perimeter
 
-b^2 = a^2 - c^2
+b^2 = c^2 - a^2
 b = n - a - c
 
-b^2 = a^2 - c^2
+b^2 = c^2 - a^2
 b^2 = n^2 - 2an + a^2 - 2nc + c^2 + 2ac
 
-a^2 - c^2 = n^2 - 2an + a^2 - 2nc + c^2 + 2ac
+c^2 - a^2 = n^2 - 2an + a^2 - 2nc + c^2 + 2ac
 
-n^2 - 2an + a^2 + a^2 - 2nc + 2ac = 0
+n^2 - 2an + 2a^2 - 2nc + 2ac = 0
 
 (n - a)^2 + a^2 - 2c(n - a) = 0
 
@@ -27,16 +27,18 @@ Therefore c = (a^2 + (n - a)^2) / 2(n - a)
 
 But we also want c to be a a whole number
 
-Therefore numerator = a^2 + (n - a)^2
+Therefore numerator   = a^2 + (n - a)^2
           denominator = 2(n - a)
 
 If numerator % denominator == 0 then c is a whole number, else go to next a
 
-Given that c is a whole number, and we know a and n, we can calculate b:
+Given that c is a whole number, and we know both a and n, we can now calculate b:
 
 a + b + c = n
 
 Therefore b = n - a - c
+
+Collect a, b and c into a list and go to next a
 |#
 
 (defpackage :pythagorean-triplet
