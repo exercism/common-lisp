398ad2bafb5d50d72b82efe10a9838dc28b6be78
Mark Simpson <verdammelt@gmail.com>
2022-06-14 10:23:54 -0400

    Update/simplify isogram example code (#655)
    Makes it clear that this is about filtering so the concept
    practices/prerequisites was also update.

--- exercises/practice/isogram/.meta/example.lisp
+++ exercises/practice/isogram/.meta/example.lisp
@@ -4,15 +4,7 @@
 
 (in-package :isogram)
 
-(defun str-list (s)
-  (assert (stringp s) (s))
-  (coerce (string-upcase s) 'list))
-
-(defun isogram-p-helper (xs)
-  (if (null xs) T
-    (and (not (and (find (car xs) "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-                   (find (car xs) (cdr xs))))
-         (isogram-p-helper (cdr xs)))))
-
 (defun isogram-p (xs)
-  (isogram-p-helper (str-list xs)))
+  (let ((chars-only (remove-if-not #'alpha-char-p xs)))
+    (string-equal chars-only
+                  (remove-duplicates chars-only :test #'char-equal))))
