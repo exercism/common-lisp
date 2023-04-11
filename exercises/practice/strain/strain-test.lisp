caadb392faba9170ee5cd650e5d4eb2732bcb410
Mark Simpson <verdammelt@gmail.com>
2023-04-11 08:05:16 -0400

    Syncing exercises from problem-specifications (#722)
    * Syncing exercise documentation files
    
    * Syncing exercise metadata
    
    * Update strain exercise exercises
    
    New TOML file uncovered two missing tests concerning lists.

--- exercises/practice/strain/strain-test.lisp
+++ exercises/practice/strain/strain-test.lisp
@@ -19,9 +19,9 @@
 
 (test keep-everything (is (equal '(1 2 3) (strain:keep #'under-10-p '(1 2 3)))))
 
-(test keep-first-last (is (equal '(1 3) (strain:keep #'oddp '(1 2 3)))))
-
 (test keep-nothing (is (equal 'nil (strain:keep #'evenp '(1 3 5 7)))))
+
+(test keep-first-last (is (equal '(1 3) (strain:keep #'oddp '(1 2 3)))))
 
 (test keep-neither-first-nor-last
  (is (equal '(2) (strain:keep #'evenp '(1 2 3)))))
@@ -32,15 +32,20 @@
     (equal '("zebra" "zombies" "zealot")
            (strain:keep #'starts-with-z-p strs)))))
 
+(test keep-lists
+ (let ((lists '( (1 2 3) (5 5 5) (5 1 2) (2 1 2) (1 5 2) (2 2 1) (1 2 5) )))
+   (is (equal '( (5 5 5) (5 1 2) (1 5 2) (1 2 5) )
+              (strain:keep #'contains-5 lists)))))
+
 (test empty-discard (is (equal 'nil (strain:discard #'under-10-p 'nil))))
 
 (test discard-everything
  (is (equal 'nil (strain:discard #'under-10-p '(1 2 3)))))
 
-(test discard-first-and-last (is (equal '(2) (strain:discard #'oddp '(1 2 3)))))
-
 (test discard-nothing
  (is (equal '(1 3 5 7) (strain:discard #'evenp '(1 3 5 7)))))
+
+(test discard-first-and-last (is (equal '(2) (strain:discard #'oddp '(1 2 3)))))
 
 (test discard-neither-first-nor-last
  (is (equal '(1 3) (strain:discard #'evenp '(1 2 3)))))
@@ -51,9 +56,17 @@
     (equal '("apple" "banana" "cherimoya")
            (strain:discard #'starts-with-z-p strs)))))
 
+
+(test discard-list
+ (let ((list '( (1 2 3) (5 5 5) (5 1 2) (2 1 2) (1 5 2) (2 2 1) (1 2 5))))
+   (is (equal '( (1 2 3) (2 1 2) (2 2 1) )
+              (strain:discard #'contains-5 list)))))
+
 (defun under-10-p (n) (< n 10))
 
 (defun starts-with-z-p (s) (char= (char s 0) #\z))
+
+(defun contains-5 (l) (member 5 l))
 
 (defun run-tests (&optional (test-or-suite 'strain-suite))
   "Provides human readable results of test run. Default to entire suite."
