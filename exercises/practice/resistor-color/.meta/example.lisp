fa0aaa73e5cfa6aeb46e582cd8ac3309364f482f
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2025-04-15 17:27:29 -0700

    Update example

--- exercises/practice/resistor-color/.meta/example.lisp
+++ exercises/practice/resistor-color/.meta/example.lisp
@@ -5,20 +5,8 @@
 
 (in-package :resistor-color)
 
-(defparameter *color-codes*
-  '(("black" . 0)
-    ("brown" . 1)
-    ("red" . 2)
-    ("orange" . 3)
-    ("yellow" . 4)
-    ("green" . 5)
-    ("blue" . 6)
-    ("violet" . 7)
-    ("grey" . 8)
-    ("white" . 9)))
-
 (defun color-code (color)
-  (cdr (assoc color *color-codes* :test #'string=)))
+  (position color (colors) :test #'string=))
 
 (defun colors ()
-  (mapcar #'car *color-codes*))
+  '("black" "brown" "red" "orange" "yellow" "green" "blue" "violet" "grey" "white"))
