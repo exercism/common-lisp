89fca152f4b18bcdae35e138a411bd0fa7013116
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2025-04-17 16:35:58 -0700

    Add `resistor-color-trio`

---
+++ exercises/practice/resistor-color-trio/.meta/example.lisp
@@ -0,0 +1,26 @@
+(defpackage :resistor-color-trio
+  (:use :cl)
+  (:export :label))
+
+(in-package :resistor-color-trio)
+
+(defun label (colors)
+    (let* ((ones (color-code (first colors)))
+           (tens (color-code (second colors)))
+           (multiplier (color-code (third colors)))
+           (value (* (+ (* ones 10) tens) 
+                      (expt 10 multiplier))))
+            (format-label value)))
+
+(defun format-label (value)
+    (cond ((< value 1e3) (format nil "~a ohms" value))
+            ((< value 1e6) (format nil "~a kiloohms" (floor value 1e3)))
+            ((< value 1e9) (format nil "~a megaohms" (floor value 1e6)))
+            ((< value 1e12) (format nil "~a gigaohms" (floor value 1e9)))
+            (t (error (format nil "Value ~a is too large to be formatted" value)))))
+
+(defun color-code (color)
+  (position color (color-values) :test #'string=))
+
+(defun color-values ()
+  '("black" "brown" "red" "orange" "yellow" "green" "blue" "violet" "grey" "white"))
