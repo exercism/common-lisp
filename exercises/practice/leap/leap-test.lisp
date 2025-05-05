1db56d52e0db2b07c98e54f252a96f15e2020117
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2025-05-05 18:52:01 -0400

    Sync `leap` tests (#877)
    [no important files changed]

--- exercises/practice/leap/leap-test.lisp
+++ exercises/practice/leap/leap-test.lisp
@@ -15,15 +15,41 @@
 ;; Define and enter a new FiveAM test-suite
 (def-suite* leap-suite)
 
-(test vanilla-leap-year (is (leap:leap-year-p 1996)))
+(test year-not-divisible-by-4-is-common-year
+    (let ((year 2015))
+      (is-false (leap:leap-year-p year))))
 
-(test any-old-year (is (not (leap:leap-year-p 1997))))
+(test year-divisible-by-2-not-divisible-by-4-is-common-year
+    (let ((year 1970))
+      (is-false (leap:leap-year-p year))))
 
-(test non-leap-even-year (is (not (leap:leap-year-p 1998))))
+(test year-divisible-by-4-not-divisible-by-100-is-leap-year
+    (let ((year 1996))
+      (is-true (leap:leap-year-p year))))
 
-(test century (is (not (leap:leap-year-p 1900))))
+(test year-divisible-by-4-and-5-is-still-a-leap-year
+    (let ((year 1960))
+      (is-true (leap:leap-year-p year))))
 
-(test exceptional-century (is (leap:leap-year-p 2400)))
+(test year-divisible-by-100-not-divisible-by-400-is-common-year
+    (let ((year 2100))
+      (is-false (leap:leap-year-p year))))
+
+(test year-divisible-by-100-but-not-by-3-is-still-not-a-leap-year
+    (let ((year 1900))
+      (is-false (leap:leap-year-p year))))
+
+(test year-divisible-by-400-is-leap-year
+    (let ((year 2000))
+      (is-true (leap:leap-year-p year))))
+
+(test year-divisible-by-400-but-not-by-125-is-still-a-leap-year
+    (let ((year 2400))
+      (is-true (leap:leap-year-p year))))
+
+(test year-divisible-by-200-not-divisible-by-400-is-common-year
+    (let ((year 1800))
+      (is-false (leap:leap-year-p year))))
 
 (defun run-tests (&optional (test-or-suite 'leap-suite))
   "Provides human readable results of test run. Default to entire suite."
