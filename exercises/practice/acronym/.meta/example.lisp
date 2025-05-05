0d30c04081d21afc59886136dc22c68d35293bff
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2025-05-05 08:50:55 -0400

    Update acronym tests (#873)

--- exercises/practice/acronym/.meta/example.lisp
+++ exercises/practice/acronym/.meta/example.lisp
@@ -1,24 +1,19 @@
-(in-package :cl-user)
 (defpackage :acronym
   (:use :cl)
   (:export :acronym))
 
 (in-package :acronym)
 
-(defun acronym (str)
-  (labels ((recur (st ls)
-    (cond
-      ((equal "" st)
-        ls)
-      ((both-case-p (elt st 0))
-       (recur (string-left-trim
-              "abcdefghijklmnopqrstuvwxyz"
-              (string-left-trim
-                "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                st))
-             (cons (elt st 0)
-                   ls)))
-      (t (recur (subseq st 1) ls)))))
-    (map 'string
-         #'char-upcase
-         (nreverse (recur str nil)))))
+(defun acronym (phrase)
+  (let ((chars ())
+        (inside-word nil))
+    (loop for char across phrase
+          do (cond
+               ((eql char #\')
+                nil)
+               ((and (alpha-char-p char) (not inside-word))
+                (push (char-upcase char) chars)
+                (setf inside-word t))
+               ((not (alpha-char-p char))
+                (setf inside-word nil))))
+    (format nil "~{~A~}" (reverse chars))))
