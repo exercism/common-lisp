03ddad47e84415e5059b12eb9f693e825bd6b962
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2025-05-06 20:15:44 -0400

    Sync `raindrops` tests (#876)

--- exercises/practice/raindrops/raindrops-test.lisp
+++ exercises/practice/raindrops/raindrops-test.lisp
@@ -15,35 +15,77 @@
 ;; Define and enter a new FiveAM test-suite
 (def-suite* raindrops-suite)
 
-(test test-1 (is (equal "1" (raindrops:convert 1))))
+(test the-sound-for-1-is-1
+    (let ((number 1))
+      (is (string= "1" (raindrops:convert number)))))
 
-(test test-3 (is (equal "Pling" (raindrops:convert 3))))
+(test the-sound-for-3-is-pling
+    (let ((number 3))
+      (is (string= "Pling" (raindrops:convert number)))))
 
-(test test-5 (is (equal "Plang" (raindrops:convert 5))))
+(test the-sound-for-5-is-plang
+    (let ((number 5))
+      (is (string= "Plang" (raindrops:convert number)))))
 
-(test test-7 (is (equal "Plong" (raindrops:convert 7))))
+(test the-sound-for-7-is-plong
+    (let ((number 7))
+      (is (string= "Plong" (raindrops:convert number)))))
 
-(test test-6 (is (equal "Pling" (raindrops:convert 6))))
+(test the-sound-for-6-is-pling-as-it-has-a-factor-3
+    (let ((number 6))
+      (is (string= "Pling" (raindrops:convert number)))))
 
-(test test-9 (is (equal "Pling" (raindrops:convert 9))))
+(test 2-to-the-power-3-does-not-make-a-raindrop-sound-as-3-is-the-exponent-not-the-base
+    (let ((number 8))
+      (is (string= "8" (raindrops:convert number)))))
 
-(test test-10 (is (equal "Plang" (raindrops:convert 10))))
+(test the-sound-for-9-is-pling-as-it-has-a-factor-3
+    (let ((number 9))
+      (is (string= "Pling" (raindrops:convert number)))))
 
-(test test-15 (is (equal "PlingPlang" (raindrops:convert 15))))
+(test the-sound-for-10-is-plang-as-it-has-a-factor-5
+    (let ((number 10))
+      (is (string= "Plang" (raindrops:convert number)))))
 
-(test test-21 (is (equal "PlingPlong" (raindrops:convert 21))))
+(test the-sound-for-14-is-plong-as-it-has-a-factor-of-7
+    (let ((number 14))
+      (is (string= "Plong" (raindrops:convert number)))))
 
-(test test-25 (is (equal "Plang" (raindrops:convert 25))))
+(test the-sound-for-15-is-plingplang-as-it-has-factors-3-and-5
+    (let ((number 15))
+      (is (string= "PlingPlang" (raindrops:convert number)))))
 
-(test test-35 (is (equal "PlangPlong" (raindrops:convert 35))))
+(test the-sound-for-21-is-plingplong-as-it-has-factors-3-and-7
+    (let ((number 21))
+      (is (string= "PlingPlong" (raindrops:convert number)))))
 
-(test test-49 (is (equal "Plong" (raindrops:convert 49))))
+(test the-sound-for-25-is-plang-as-it-has-a-factor-5
+    (let ((number 25))
+      (is (string= "Plang" (raindrops:convert number)))))
 
-(test test-52 (is (equal "52" (raindrops:convert 52))))
+(test the-sound-for-27-is-pling-as-it-has-a-factor-3
+    (let ((number 27))
+      (is (string= "Pling" (raindrops:convert number)))))
 
-(test test-105 (is (equal "PlingPlangPlong" (raindrops:convert 105))))
+(test the-sound-for-35-is-plangplong-as-it-has-factors-5-and-7
+    (let ((number 35))
+      (is (string= "PlangPlong" (raindrops:convert number)))))
 
-(test test-12121 (is (equal "12121" (raindrops:convert 12121))))
+(test the-sound-for-49-is-plong-as-it-has-a-factor-7
+    (let ((number 49))
+      (is (string= "Plong" (raindrops:convert number)))))
+
+(test the-sound-for-52-is-52
+    (let ((number 52))
+      (is (string= "52" (raindrops:convert number)))))
+
+(test the-sound-for-105-is-plingplangplong-as-it-has-factors-3-5-and-7
+    (let ((number 105))
+      (is (string= "PlingPlangPlong" (raindrops:convert number)))))
+
+(test the-sound-for-3125-is-plang-as-it-has-a-factor-5
+    (let ((number 3125))
+      (is (string= "Plang" (raindrops:convert number)))))
 
 (defun run-tests (&optional (test-or-suite 'raindrops-suite))
   "Provides human readable results of test run. Default to entire suite."
