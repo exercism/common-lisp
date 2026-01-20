ab1d0e328346e5ce3c0a010e61456bc4cc8a7e05
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2026-01-19 22:42:31 -0800

    Apply code review

--- exercises/practice/relative-distance/.meta/example.lisp
+++ exercises/practice/relative-distance/.meta/example.lisp
@@ -4,51 +4,38 @@
 
 (in-package :relative-distance)
 
-(defun degree-of-separation (family-tree person-a person-b)
+(defun build-graph (family-tree)
   (let ((neighbors (make-hash-table :test 'equal)))
+    (loop for (parent . children) in family-tree do
+          (unless (gethash parent neighbors)
+            (setf (gethash parent neighbors)
+                  (make-hash-table :test 'equal)))
+          (loop for child in children do
+            (unless (gethash child neighbors)
+              (setf (gethash child neighbors)
+                    (make-hash-table :test 'equal)))
+            (setf (gethash child (gethash parent neighbors)) t
+                  (gethash parent (gethash child neighbors)) t))
+          (loop for (child-a . rest) on children do
+                (loop for child-b in rest do
+                  (setf (gethash child-a (gethash child-b neighbors)) t
+                        (gethash child-b (gethash child-a neighbors)) t))))
+    neighbors))
 
-    (dolist (entry family-tree)
-            (let ((parent (car entry))
-                  (children (cdr entry)))
-
-              (unless (gethash parent neighbors)
-                (setf (gethash parent neighbors) (make-hash-table :test 'equal)))
-
-              (dolist (child children)
-                      (unless (gethash child neighbors)
-                        (setf (gethash child neighbors) (make-hash-table :test 'equal)))
-                      (setf (gethash child (gethash parent neighbors)) t)
-                      (setf (gethash parent (gethash child neighbors)) t))
-
-              (dolist (child-a children)
-                      (dolist (child-b children)
-                              (unless (equal child-a child-b)
-                                (setf (gethash child-a (gethash child-b neighbors)) t)
-                                (setf (gethash child-b (gethash child-a neighbors)) t))))))
-
-    (unless (and (gethash person-a neighbors) (gethash person-b neighbors))
-      (return-from degree-of-separation nil))
-
-
-    (let ((queue (list (cons person-a 0)))
-          (visited (make-hash-table :test 'equal)))
-
-      (setf (gethash person-a visited) t)
-
-      (loop while queue do
-            (let* ((current (pop queue))
-                   (current-person (car current))
-                   (current-degree (cdr current)))
-
-              (when (equal current-person person-b)
-                (return-from degree-of-separation current-degree))
-
-              (let ((current-neighbors (gethash current-person neighbors)))
-                (when current-neighbors
-                  (maphash (lambda (neighbor _)
-                             (unless (gethash neighbor visited)
-                               (setf (gethash neighbor visited) t)
-                               (setf queue (append queue (list (cons neighbor (1+ current-degree)))))))
-                           current-neighbors))))))
-
-    nil))
+(defun degree-of-separation (family-tree person-a person-b)
+  (let ((neighbors (build-graph family-tree)))
+    (when (and (gethash person-a neighbors)
+               (gethash person-b neighbors))
+      (let ((queue (list (cons person-a 0)))
+            (visited (make-hash-table :test 'equal)))
+        (setf (gethash person-a visited) t)
+        (loop while queue
+              for (current-person . current-degree) = (pop queue)
+              if (equal current-person person-b)
+                return current-degree
+              do (maphash (lambda (neighbor val)
+                            (declare (ignore val))
+                            (unless (gethash neighbor visited)
+                              (setf (gethash neighbor visited) t
+                                    queue (append queue (list (cons neighbor (1+ current-degree)))))))
+                          (gethash current-person neighbors)))))))
